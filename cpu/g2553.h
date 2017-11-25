6efd896d871d97eda1b75180a3d9345564270e5a
Jan Brittenson <bson@rockgarden.net>
2017-11-25 00:06:17 -0800

    Add GPIO Port and Pin wrappers

--- cpu/g2553.h
+++ cpu/g2553.h
@@ -4,10 +4,12 @@
 #include "common.h"
 #include "timer.h"
 #include "usci_b.h"
+#include "gpio.h"
 
-typedef UCB<msp430::UCB0STAT, msp430::UCB0CTL0, msp430::UCB0CTL1, msp430::UCB0BR0, msp430::UCB0BR1,
-            msp430::UCB0I2CIE, msp430::UCB0RXBUF, msp430::UCB0TXBUF, msp430::UCB0I2COA, msp430::UCB0I2CSA,
-            msp430::UC0IFG> UCB0;
+typedef UCB<msp430::UCB0STAT, msp430::UCB0CTL0, msp430::UCB0CTL1, 
+            msp430::UCB0BR0, msp430::UCB0BR1, msp430::UCB0I2CIE, 
+            msp430::UCB0RXBUF, msp430::UCB0TXBUF, msp430::UCB0I2COA, 
+            msp430::UCB0I2CSA, msp430::UC0IFG> UCB0;
 
 typedef TimerA3<msp430::TA0CTL, msp430::TA0R, msp430::TA0CCTL0,
                 msp430::TA0CCR0, msp430::TA0CCTL1, msp430::TA0CCR1,
@@ -16,5 +18,17 @@ typedef TimerA3<msp430::TA0CTL, msp430::TA0R, msp430::TA0CCTL0,
 // System needs a timer someplace.
 typedef TimerA3_0 SysTimer;
 extern SysTimer _sysTimer;
+
+typedef Port<msp430::P1IN, msp430::P1OUT, msp430::P1DIR, msp430::P1SEL,
+             msp430::P1SEL2, msp430::P1REN, msp430::P1IFG, msp430::P1IES,
+             msp430::P1IE> Port1;
+
+typedef Port<msp430::P2IN, msp430::P2OUT, msp430::P2DIR, msp430::P2SEL,
+             msp430::P2SEL2, msp430::P2REN, msp430::P2IFG, msp430::P2IES,
+             msp430::P2IE> Port2;
+
+// 28 pin devices only
+//typedef Port<msp430::P2IN, msp430::P2OUT, msp430::P2DIR, msp430::P2SEL,
+//             msp430::P2SEL2, msp430::P2REN> Port3;
 
 #endif // _G2553_H_
---
+++ gpio.h
@@ -0,0 +1,78 @@
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
+// Port 1,2 has all of registers, while P3-P7 lack the edge interrupt
+// support and lack the last three.
+
+template <volatile uint8_t& _IN,
+          volatile uint8_t& _OUT,
+          volatile uint8_t& _DIR,
+          volatile uint8_t& _SEL,
+          volatile uint8_t& _SEL2,
+          volatile uint8_t& _REN,
+          volatile uint8_t& _IFG,
+          volatile uint8_t& _IES,
+          volatile uint8_t& _IE>
+class Port {
+public:
+    ACCESSOR(volatile uint8_t&, getPIN, _IN);
+    ACCESSOR(volatile uint8_t&, getPOUT, _OUT);
+    ACCESSOR(volatile uint8_t&, getPDIR, _DIR);
+    ACCESSOR(volatile uint8_t&, getPSEL, _SEL);
+    ACCESSOR(volatile uint8_t&, getPSEL2, _SEL2);
+    ACCESSOR(volatile uint8_t&, getPREN, _REN);
+    ACCESSOR(volatile uint8_t&, getPIFG, _IFG);
+    ACCESSOR(volatile uint8_t&, getPIES, _IES);
+    ACCESSOR(volatile uint8_t&, getPIE, _IE);
+
+    Port() { }
+};
+
+
+// PIN abstraction, for convenience and to simplify pin swapping
+// without having to prune significant bodies of code and change
+// e.g. P1xxx to P2xxx.
+template <typename PORT,
+          uint8_t  _BIT>
+class Pin: public PORT {
+public:
+    enum { MASK = 1 << _BIT };
+
+    enum Direction {
+        OUTPUT = MASK,
+        INPUT  = 0
+    };
+
+    enum Select {
+        SEL0 = 0b00,
+        SEL1 = 0b01,
+        SEL2 = 0b10,
+        SEL3 = 0b11,
+    };
+
+    enum Pullup {
+        ENABLE = MASK,
+        DISABLE = 0
+    };
+
+    // Pin.IN gettor
+    static force_inline uint8_t getIN() { return PORT.P_IN & MASK; }
+
+    // Set pin state
+    static force_inline void set(bool state) {
+        PORT.P_OUT = (PORT.P_OUT & ~MASK) | (state ? MASK : 0);
+    }
+
+    // Configure
+    static force_inline void config(Direction dir,
+                                    Select sel,
+                                    Pullup ren) {
+        PORT.P_DIR  = (PORT.P_DIR  & ~MASK) | uint8_t(dir);
+        PORT.P_SEL  = (PORT.P_SEL  & ~MASK) | (uint8_t(sel) & 1);
+        PORT.P_SEL2 = (PORT.P_SEL2 & ~MASK) | ((uint8_t(sel) >> 1) & 1);
+        PORT.P_REN  = (PORT.P_REN  & ~MASK) | uint8_t(ren);
+    };
+                               
+};
+
+#endif // _GPIO_H_
