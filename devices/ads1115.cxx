9ce07f77f188fbcbb926f37c26165988ff295f76
Jan Brittenson <bson@rockgarden.net>
2017-12-01 21:21:54 -0800

    Fix up and test ADS1115

--- devices/ads1115.cxx
+++ devices/ads1115.cxx
@@ -8,10 +8,47 @@ namespace ads1115 {
 template <typename Bus, typename Device>
 void ADC<Bus,Device>::start_single_read() {
     if (Device::start_write(REG_CONF)) {
-        Device::write((_config >> 8) | 1);  // Bit 8 => single shot conversion
+        Device::write((_config >> 8) | 0x81);  // Bit 8 => single shot conversion
         Device::write(_config & 0xff);
         Device::write_done();
     }
+}
+
+template <typename Bus, typename Device>
+bool ADC<Bus,Device>::wait_conv() {
+	static const uint8_t to[] = {1000/8, 1000/16, 1000/32, 1000/64, 1000/128,
+								1000/250, 1000/475, 1000/7};
+	uint8_t data_hi;
+	uint8_t data_lo;
+
+	Device::transmit(REG_CONF);
+	const SysTimer::Future deadline = _sysTimer.future(TIMER_MSEC(to[_sps]));
+	bool ok = true;
+	if (Device::start_read(&data_hi) && Device::read(&data_lo)) {
+		while ((data_hi & (CONF_OS >> 8)) && !_sysTimer.due(deadline)) {
+			_sysTimer.delay(TIMER_USEC(500));
+			if (!Device::read(&data_hi) || !Device::read(&data_lo)) {
+				ok = false;
+				break;
+			}
+		}
+		Device::read_done();
+		return ok && !(data_hi & (CONF_OS >> 8));
+	}
+	return false;
+}
+
+template <typename Bus, typename Device>
+uint16_t ADC<Bus,Device>::read_conv() {
+	uint8_t data_hi;
+	uint8_t data_lo;
+
+	Device::transmit(REG_CONV);
+	if (Device::start_read(&data_hi) && Device::read(&data_lo)) {
+		Device::read_done();
+	}
+
+	return (data_hi << 8) | data_lo;
 }
 
 
--- devices/ads1115.h
+++ devices/ads1115.h
@@ -7,31 +7,26 @@ namespace ads1115 {
 
 template <typename Bus, typename Device>
 class ADC: public Device {
-private:
-    uint16_t _config;
-
 public:
-    enum {
-        // Registers (comparator is not used by this code)
-        REG_CONV = 0,   // Conversion register
-        REG_CONF = 1,   // Config register
-        REG_LO_THR = 2, // Comparator low threshold
-        REG_HI_THR = 3, // Comparator high threshold
-
+    enum Channel {
         // Channels
         IN0 = 0,
         IN1 = 1,
         IN2 = 2,
-        IN3 = 3,
+        IN3 = 3
+    };
 
+    enum FSR {
         // PGA settings (Full Scale Range)
         FSR_6_144 = 0,   // 6.144V
         FSR_4_096 = 1,
         FSR_2_048 = 2,
         FSR_1_024 = 3,
         FSR_0_512 = 4,
-        FSR_0_256 = 5,  // 0.256V for values 5,6,7
+        FSR_0_256 = 5  // 0.256V for values 5,6,7
+    };
 
+    enum SPS {
         // Sample rates
         SPS_8 = 0,
         SPS_16 = 1,
@@ -41,6 +36,30 @@ public:
         SPS_250 = 5,
         SPS_475 = 6,
         SPS_860 = 7
+    };
+
+private:
+    uint16_t _config;
+    SPS _sps;
+
+public:
+    enum {
+        // Registers (comparator is not used by this code)
+        REG_CONV = 0,   // Conversion register
+        REG_CONF = 1,   // Config register
+        REG_LO_THR = 2, // Comparator low threshold
+        REG_HI_THR = 3, // Comparator high threshold
+
+		// Config bits
+		CONF_OS = 1 << 15,
+		CONF_MUX = 1 << 12, // 12:14
+		CONF_PGA = 1 << 9,  // 9:11
+		CONF_MODE = 1 << 8,
+		CONF_DR = 1 << 5,   // 5:7
+		CONF_COMP_MODE = 1 << 4,
+		CONF_COMP_POL = 1 << 3,
+		CONF_COMP_LAT = 1 << 2,
+		CONF_COMP_QUE = 1   // 0:1
     };
 
     ADC(Bus& bus, uint8_t addr)
@@ -54,12 +73,19 @@ public:
     void force_inline init() { }
 
     // Select input, gain, and SPS
-    void config(uint8_t channel, uint8_t fsr, uint8_t sps) {
-        _config = ((_channel | 0b100) << 12) | (fsr << 9) | (_sps << 5);
+    void config(Channel channel, FSR fsr, SPS sps) {
+    	    _sps = sps;
+        _config = ((channel | 0b100) << 12) | (fsr << 9) | (_sps << 5);
     }
 
-    // One-shot read
+    // Start one-shot read
     void start_single_read();
+
+    // Wait for conversion done
+    bool wait_conv();
+
+    // Read conversion register
+    uint16_t read_conv();
 
 private:
     ADC(const ADC&);
